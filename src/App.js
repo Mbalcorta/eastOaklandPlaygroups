3aa82a85d8c9ae5e7690af341fb6509ffa832efa
Mateo Balcorta <misterrattail@gmail.com>
2018-06-02 22:09:44 -0700

    Update to access data from db.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import React, { Component} from "react";
-import "./style.css";
+import React, { Component} from "react"
+import "./style.css"
+import db from "./db.js"
+
+
 
 class App extends Component{
+  constructor(props){
+    super(props)
+
+    this.state = {
+      events: []
+    }
+  }
+
+  componentDidMount(){
+    db.ref().on("value", (snapshot)  => {
+      const allEvents = snapshot.val()
+      this.setState({ events: [allEvents].concat(this.state.events) });
+    }, function (error) {
+      console.log("Error: " + error.code);
+    });
+  }
+  
   render(){
+    let allEvents = ''
+    if(this.state.events.length > 0 ) {
+      allEvents = this.state.events[0]['libraryEvents']['melrose_library'][1]['info']
+    }
+
     return(
       <div className="App">
-        <h1> Hello, World! </h1>
+        <h1>{ allEvents }</h1>
       </div>
     );
   }
