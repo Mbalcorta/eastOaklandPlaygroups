b56be6dddf48779579b28960dd470575724cf9f1
Mateo Balcorta <misterrattail@gmail.com>
2018-06-12 13:02:00 -0700

    Updated day prefix for the first of the month.

--- src/js/getDate.js
+++ src/js/getDate.js
@@ -7,7 +7,10 @@ const currentMonth = today.getMonth()
 const formattedCurrentMonth = ("0" + (currentMonth + 1)).slice(-2)
 const currentYear = today.getFullYear()
 const monthList = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
-
-// const date = day+', '+monthList[currentMonth]+' '+currentDate+'th, '+currentYear
-const date = 'Wednesday'+', '+monthList[currentMonth]+' '+'13'+'th, '+currentYear
+let dayPrefix = 'th, '
+if(currentDate === 1){
+    dayPrefix = 'st, '
+}
+const date = day+', '+monthList[currentMonth]+' '+currentDate+dayPrefix+currentYear
+// const date = 'Tuesday'+', '+monthList[currentMonth]+' '+currentDate+dayPrefix+currentYear
 export default date
