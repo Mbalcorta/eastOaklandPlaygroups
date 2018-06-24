2903b0e868fc9d60aea0247fef7de3c6b4a4251d
Mateo Balcorta <misterrattail@gmail.com>
2018-06-24 15:39:25 -0700

    Update date to have correct prefix.

--- src/js/getDate.js
+++ src/js/getDate.js
@@ -8,9 +8,14 @@ const formattedCurrentMonth = ("0" + (currentMonth + 1)).slice(-2)
 const currentYear = today.getFullYear()
 const monthList = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 let dayPrefix = 'th, '
-if(currentDate === 1){
+if(currentDate === 1 || currentDate === 31){
     dayPrefix = 'st, '
+} else if(currentDate === 2){
+    dayPrefix = 'nd, '
+} else if(currentDate === 3){
+    dayPrefix = 'rd, '
 }
 const date = day+', '+monthList[currentMonth]+' '+currentDate+dayPrefix+currentYear
-// const date = 'Tuesday'+', '+monthList[currentMonth]+' '+currentDate+dayPrefix+currentYear
+// const date = 'Thursday'+', '+'May'+' '+24+dayPrefix+currentYear
+// const date = 'Wednesday'+', '+monthList[currentMonth]+' '+27+dayPrefix+currentYear
 export default date
