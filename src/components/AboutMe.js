5d3ce8ee31b4168887940883202d985957bb384d
Mateo Balcorta <misterrattail@gmail.com>
2018-09-29 20:36:57 -0700

    Add about me component.

---
+++ src/components/AboutMe.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const AboutMe = () => {
+ return(
+  <div className="AboutMe">
+    <h1> About Me</h1>
+    <p>I'm a papa with a two year old living in east oakland wanting to make kids activities accessible to all parents in the area. If you know of great activities in the east oakland area please reach out. Thank you! </p>
+    <p><a href="mailto:mateo.balcorta@gmail.com">Mateo.balcorta@gmail.com</a></p>
+  </div>
+ )
+}
+
+module.exports = AboutMe;
