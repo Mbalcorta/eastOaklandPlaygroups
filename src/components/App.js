05758dbea45b0516e3f17b6c1852a499cddc71d5
Mateo Balcorta <misterrattail@gmail.com>
2018-09-29 20:38:06 -0700

    Add components to app.

--- src/components/App.js
+++ src/components/App.js
@@ -2,18 +2,25 @@ import React, { Component } from 'react'
 import '../styles/App.css'
 import '../styles/index.css'
 import {apiKey} from '../../config/keys.js'
-import date from '../js/getDate.js'
+
 // import the Google Maps API Wrapper from google-maps-react
 import { GoogleApiWrapper } from 'google-maps-react' 
 // import child component
 import MapContainer from './MapContainer'
+import Header from './Header';
+import About from './About';
+import Resources from './Resources';
+import AboutMe from './AboutMe';
+
 class App extends Component {
   render() {
     return (
-      <div className="header">
-        <h1> East Oakland Kids Activities </h1>
-        <h3>{ date }</h3>
+      <div className="container">
+        <Header/>
+        <About />
         <MapContainer google={this.props.google} />
+        <Resources />
+        <AboutMe />
       </div>
     );
   }
