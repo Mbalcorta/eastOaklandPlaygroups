a34ee499a8bbacd0ae38d96bf6bf02b16ce06caa
Mateo Balcorta <misterrattail@gmail.com>
2018-09-30 13:50:14 -0700

    Added more info under resources.

--- src/components/Resources.js
+++ src/components/Resources.js
@@ -4,8 +4,10 @@ const Resources = () => {
  return(
   <div className="resources">
     <h1> Resources </h1>
-    <p>Get free museum passes with <a href="https://oaklandlibrary.discoverandgo.net/" target="_blank">discover go</a>, just need an oakland library card which is free</p>
-    <p>EBT cardholders get in free or very low cost to many museums with <a href="http://childrensmuseums.org/participating-museums" target="_blank">Museums for All </a></p>
+    <p><a href="https://oaklandlibrary.discoverandgo.net/" target="_blank">discover go</a> is a program provided by your library that offers free and low-cost passes for museums, science centers, zoos, theatres, and other cultural destinations. You must meet your library's minimum age requirement and live within your library's service area to use the Discover & Go Service.</p>
+    
+    <p><a href="http://childrensmuseums.org/participating-museums" target="_blank">Museums for All </a>
+    Offer individual admission fees ranging from free to $3.00 (USD) to individuals and families presenting a Supplemental Nutrition Assistance Program (SNAP) Electronic Benefits Transfer (EBT) card, and a valid form of photo ID. Museums for All admission rates must be offered for up to four individuals per EBT card. This admission rate must be available during all normal operating hours.</p>
   </div>
  )
 }
