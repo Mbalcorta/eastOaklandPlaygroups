033ceaff7ff735ffb8785122f27799975e72311f
Mateo Balcorta <misterrattail@gmail.com>
2018-09-29 20:37:28 -0700

    Add resources component.

---
+++ src/components/Resources.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Resources = () => {
+ return(
+  <div className="resources">
+    <h1> Resources </h1>
+    <p>Get free museum passes with <a href="https://oaklandlibrary.discoverandgo.net/" target="_blank">discover go</a>, just need an oakland library card which is free</p>
+    <p>EBT cardholders get in free or very low cost to many museums with <a href="http://childrensmuseums.org/participating-museums" target="_blank">Museums for All </a></p>
+  </div>
+ )
+}
+
+module.exports = Resources;
