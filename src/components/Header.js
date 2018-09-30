ddc3ec3a3b94de1510a0f1b820603255e4e93842
Mateo Balcorta <misterrattail@gmail.com>
2018-09-30 14:38:01 -0700

    Added google translate widget.

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,13 @@ import React from 'react';
 
 const Header = () => {
  return(
+ 
   <div className="header">
+   <div id="google_translate_element"></div>
+    <div className="headerContent">
     <h1> East Oakland Kids Activities </h1>
-    <p>Resources and no cost activites for families in our own backyards</p>
+    <p>Resources and no cost activites for families in our own backyard</p>
+    </div>
   </div>
  )
 }
