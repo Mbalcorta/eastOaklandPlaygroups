2b36f908fbc34bdbe6415a94568460d9addebe77
Mateo Balcorta <misterrattail@gmail.com>
2018-09-30 13:49:06 -0700

    Update about me section.

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -4,7 +4,7 @@ const AboutMe = () => {
  return(
   <div className="AboutMe">
     <h1> About Me</h1>
-    <p>I'm a papa with a two year old living in east oakland wanting to make kids activities accessible to all parents in the area. If you know of great activities in the east oakland area please reach out. Thank you! </p>
+    <p>I'm a latino papa living with my partner, our two year old, two dogs and four chickens in east oakland. I've created this site wanting to make kids activities accessible to all parents in the area. If you know of great activities in the east oakland area please reach out. Thank you! </p>
     <p><a href="mailto:mateo.balcorta@gmail.com">Mateo.balcorta@gmail.com</a></p>
   </div>
  )
